f98101ce3423aac018f261ae057da91b25f3e636
건성윤 <wave624@naver.com>
2026-03-29 14:10:31 +0900

    chore: 임시 파일 정리

---
+++ fix_og_path.py
@@ -0,0 +1,10 @@
+﻿p = r'E:\아진네트웍스\Claude Code\블로그 제작\blog_agent\_includes\og-tags.html'
+with open(p, 'r', encoding='utf-8') as f:
+    html = f.read()
+html = html.replace(
+    "/assets/images/og-default.png",
+    "/assets/img/og-default.png"
+)
+with open(p, 'w', encoding='utf-8') as f:
+    f.write(html)
+print("✅ OG 이미지 경로 수정 완료")
